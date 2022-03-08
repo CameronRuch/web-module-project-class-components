36b34613d86342c93d0adc549ed425b053618b26
CameronRuch <cameron.ruch@yahoo.com>
2022-03-08 06:23:35 -0600

    added baseline dom markup

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,39 @@
 import React from 'react'
 
+let idx = 0;
+const getIdx = () => ++idx;
+
+const todos = [
+  {id: getIdx(), todo: ''}
+]
+
+const initialState = {
+  errorMessage: '',
+  todo: ''
+}
+
+
+
+
 export default class App extends React.Component {
+  state = initialState;
+
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos:</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Learn React</li>
+          <li>Have Fun</li>
+        </ul>  
+
+        <form>
+          <input/>
+          <button>Submit</button>
+        </form>     
+
+        <button>Clear</button>
       </div>
     )
   }
