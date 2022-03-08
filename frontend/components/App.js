39356af174e04214127d2167c2316ea5b2793dfd
CameronRuch <cameron.ruch@yahoo.com>
2022-03-08 16:18:33 -0600

    added button to toggle completed and add new todos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -24,10 +24,37 @@ export default class App extends React.Component {
     }
   }
 
+  handleNew = (name) => {
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      ...this.state, todos: [...this.state.todos, newTodo]
+    });
+  }
+
   handleCompleted = () => {
     this.setState({
       ...this.state, todos: this.state.todos.filter(todo => {
         return  (todo.completed === false)
+      })
+    })
+  }
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state, 
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo, completed: !todo.completed
+          }
+        } 
+          return todo
+        
       })
     })
   }
@@ -37,8 +64,8 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>Todos:</h1>
-        <TodoList todos={todos} />
-        <Form />
+        <TodoList handleToggle={this.handleToggle} todos={todos} />
+        <Form handleNew={this.handleNew}/>
         <button onClick={this.handleCompleted}>Clear Completed</button>
       </div>
     )
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
 export default class Form extends React.Component {
-  render() {
 
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+  }
+
+handleSubmit = (e) => {
+  e.preventDefault();
+  this.props.handleNew(this.state.input);
+
+}
+
+handleChange = (e) => {
+  this.setState({
+    ...this.state, input: e.target.value
+  })
+}
+
+  render() {
+    
     return (
 
       <form>
 
-        <input />
-        <button>Submit</button>
+        <input onChange={this.handleChange}/>
+        <button onClick={this.handleSubmit}>Submit</button>
 
       </form>
 
